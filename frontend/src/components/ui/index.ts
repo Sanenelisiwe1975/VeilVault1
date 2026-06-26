2b468af09cb3ea27bdf482689fd19147dd6dc59b
sanenelisiwe1975 <Sanelisiwesimatu@gmail.com>
2026-06-26 13:52:48 +0200

    index

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -3,3 +3,4 @@ export { GradientText   } from "./GradientText";
 export { GradientButton } from "./GradientButton";
 export { Badge, RiskDot, PrivacyBars, APYBadge } from "./Indicators";
 export { ConnectModal } from "./ConnectModal";
+export { CollapsibleSection } from "./CollapsibleSection";
